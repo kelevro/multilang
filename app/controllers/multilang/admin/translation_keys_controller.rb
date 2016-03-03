93d9fc7c3c75d9c1e487695d379bff373568d94f
Anton Holovko <anton.holovko.b@gmail.com>
2016-03-03 14:33:27 +0200

    add dirty state

--- app/controllers/multilang/admin/translation_keys_controller.rb
+++ app/controllers/multilang/admin/translation_keys_controller.rb
@@ -1,13 +1,15 @@
 class Multilang::Admin::TranslationKeysController < Multilang::Admin::ApplicationController
+
+  before_action :set_searcher, only: [:create]
+
   def create
     authorize! :create, :multilang_translation_key
-    translation_key = Multilang::TranslationKey.new translation_key_params
-    if translation_key.save!
-      flash[:success] = 'Translation key added successfully'
-    else
-      flash[:error] = 'Cannot add translation key'
+    @translation_key = Multilang::TranslationKey.new translation_key_params
+    @translation_key.save
+    respond_to do |format|
+      format.html { redirect_to admin_translations_path }
+      format.js
     end
-    redirect_to admin_translations_path
   end
 
   def destroy
